d8182d16f99b6fb0c689e92fd736d9aebe3d58ee
taniamalashko <sashaand.tanya8@gmail.com>
2023-05-12 13:54:46 +0300

    Homework 12

--- homeWork12/homeWork12.js
+++ homeWork12/homeWork12.js
@@ -1,16 +1,5 @@
-//price of fruits/vegetables
-cabbagePrice = 8;
-avocadoPrice = 30;
-tomatoPrice = 10;
-grapesPrice = 20;
-raspberryPrice = 25;
-coconutPrice = 50;
-
-//Final price in card
-finalPrice = 0;
-
-//String with path to the image
-thePathToTheImage = '/Hillel_Homework/homeWork12/images/';
+productsCategories = ['vegetables', 'fruits'];
+prices = [[8, 30, 10], [20,25,50]];
 
 //Asking the user for the time of year
 do {
@@ -30,57 +19,32 @@ do {
     productCategory = productCategory.replaceAll(' ', '').toLowerCase();
 } while (productCategory !== 'fruits' && productCategory !== 'vegetables');
 
-//We ask the user which product from the selected category he buys
 switch (productCategory) {
-    case 'fruits':
-        do {
-            product = prompt('What kind of fruit do you want to buy (grapes/raspberry/coconut)?');
-            while (!product) {
-                product = prompt('What kind of fruit do you want to buy (grapes/raspberry/coconut)?');
-            }
-            product = product.replaceAll(' ', '').toLowerCase();
-        } while (product !== 'grapes' && product !== 'raspberry' && product !== 'coconut');
-        thePathToTheImage += '/fruits';
-        break
     case 'vegetables':
-        do {
-            product = prompt('What kind of vegetables do you want to buy (cabbage/avocado/tomato)?');
-            while (!product) {
-                product = prompt('What kind of vegetables do you want to buy (cabbage/avocado/tomato)?');
-            }
-            product = product.replaceAll(' ', '').toLowerCase();
-        } while (product !== 'cabbage' && product !== 'avocado' && product !== 'tomato');
-        thePathToTheImage += '/vegetables';
+        products = ['cabbage', 'avocado', 'tomato'];
+        prices = prices[0];
+        break
+    case 'fruits':
+        products = ['grapes', 'raspberry', 'coconut'];
+        prices = prices[1];
         break
 }
 
-//We enter data into the final price
-switch (product) {
-    case 'grapes':
-        finalPrice += grapesPrice;
-        thePathToTheImage += '/grapes.svg'
-        break
-    case 'raspberry':
-        finalPrice += raspberryPrice;
-        thePathToTheImage += '/raspberry.svg'
-        break
-    case 'coconut':
-        finalPrice += coconutPrice;
-        thePathToTheImage += '/coconut.svg'
-        break
-    case 'cabbage':
-        finalPrice += cabbagePrice;
-        thePathToTheImage += '/cabbage.svg'
-        break
-    case 'avocado':
-        finalPrice += avocadoPrice;
-        thePathToTheImage += '/avocado.svg'
-        break
-    case 'tomato':
-        finalPrice += tomatoPrice;
-        thePathToTheImage += '/tomato.svg'
-        break
-}
+//We ask the user which product from the selected category he buys
+do {
+    product = prompt(`What kind of ${productCategory} do you want to buy (${products.join('/')})?`);
+    while (!product) {
+        product = prompt(`What kind of ${productCategory} do you want to buy (${products.join('/')})?`);
+    }
+    product = product.replaceAll(' ', '').toLowerCase();
+} while (!products.includes(product));
+
+//Ask how many units of the product the customer wants to buy and calculate the final price
+do {
+    countOfProducts = parseInt(prompt('How many items do you want to purchase (quantity must be more than one)?'));
+} while (isNaN(countOfProducts) || !countOfProducts || countOfProducts === 1);
+
+finalPrice = prices[products.indexOf(product)]*countOfProducts
 
 switch (season) {
     case 'winter':
@@ -91,20 +55,17 @@ switch (season) {
         break
 }
 
-//Ask how many units of the product the customer wants to buy and calculate the final price
-do {
-    countOfProducts = parseInt(prompt('How many items do you want to purchase (quantity must be more than one)?'));
-} while (isNaN(countOfProducts) || !countOfProducts || countOfProducts === 1);
-
-finalPrice *= countOfProducts;
+//Specify the path to the image
+imagePath = `/Hillel_Homework/homeWork12/images/${productCategory}/${product}.svg`;
 
 //We form a card with an order and a price
 document.write(`<div class="product" align="center"">
-                <img src="${thePathToTheImage}" alt="${product}" width="100" height="100">
+                <img src="${imagePath}" alt="${product}" width="100" height="100">
                 <p>Selected product: <b>${product}</b></p>
                 <p>Count of ${product}: <b>${countOfProducts}</b></p>
                 <p>Selected period: <b>${season}</b></p>
                 <p>Selected category: <b>${productCategory}</b></p>
                 <p>Final sum: <b>${finalPrice} UAH</b></p>
                 </div>`);
+
 
