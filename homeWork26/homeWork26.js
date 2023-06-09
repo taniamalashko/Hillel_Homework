4e34762cbca52ffb2d543882f64098b08ed83543
taniamalashko <sashaand.tanya8@gmail.com>
2023-06-09 14:53:46 +0300

    Homework 26

--- homeWork26/homeWork26.js
+++ homeWork26/homeWork26.js
@@ -19,67 +19,42 @@ let dcHeroes = [
 	}
 ];
 
-let heroes = [
-	{
-		name: "Superman"
-	},
-	{
-		name: "Batman"
-	},
-	56,
-	{
-		name: "Deadpool"
-	},
-	{
-		name: "Thor"
-	},
-	[89, 'kk'],
-	{
-		name: "Spider Man"
-	}
-];
 
 
 Array.prototype.heroesRender = function(universe){
 	let universeName = universe.replaceAll(' ', '').toLowerCase();
 
-    if (['dc', 'marvel'].includes(universeName)) {
-		heroesArray = universeName === 'dc' ? dcHeroes : marvelHeroes;
+	let TRs = [];
 
-		let TRs = [];
-
-		this.forEach(function(item){
-			if (heroesArray.some(function(element){
-				return element.name === item.name;
-			})) {
-				TRs.push(`
-				<tr>
-			   	<td>${item.name}</td>
-			   	<td>
-			    <img src="/homeWork26/${universeName}/${item.name.replaceAll(' ', '').toLowerCase()}.svg">
-			   	</td>
-			    </tr>`);
-			}
-		})
-		
-		if (TRs.length !== 0) {
-			document.write(`<table>
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>Icon</th>
-				</tr>
-			</thead>
-			<tbody>
-				${TRs.join('')}
-			</tbody>
-		</table>`);
-		} else {
-			console.log(`There is no heroes from this universe.`);
+	this.forEach(function(item){
+		if (item.name && typeof item.name === 'string') {
+			imgName = item.name.replaceAll(' ', '').toLowerCase();
+			TRs.push(`
+			<tr>
+			<td>${item.name}</td>
+			<td>
+			<img src="/homeWork26/${universeName}/${imgName}.svg">
+			</td>
+			</tr>`);
 		}
-    } else {
-        console.log('There is no such universe.');
-    }
+	})
+	
+	if (TRs.length !== 0) {
+		document.write(`<table>
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Icon</th>
+								</tr>
+							</thead>
+							<tbody>
+								${TRs.join('')}
+							</tbody>
+						</table>`);
+	} else {
+		console.log(`There is no heroes .`);
+	}
 }
 
-heroes.heroesRender('marvel');
+dcHeroes.heroesRender('dc');
+marvelHeroes.heroesRender('marvel');
